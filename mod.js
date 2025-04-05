64c8572764a7948103bf1c7315770c763f56a36d
krafta. <krafta.visio@gmail.com>
2025-04-05 09:22:50 +0800

    Delete mod.js

--- mod.js
+++
@@ -1,3 +0,0 @@
-export * from "./deserialize.js"; // Mengekspor semua fungsi dan variabel dari file deserialize.js
-export * from "./serialize.js";   // Mengekspor semua fungsi dan variabel dari file serialize.js
-export * from "./types.js";       // Mengekspor semua fungsi dan variabel dari file types.js
